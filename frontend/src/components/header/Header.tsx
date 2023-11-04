3a8ff86aeda348ba9dafad747376f52c8913e236
batyabetter <b.0nashkul@gmail.com>
2023-11-04 22:50:22 +0600

    qwfqf

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -104,7 +104,7 @@ function Header() {
                         textDecoration: "none",
                      }}
                   >
-                     Alisher loh
+                     Alisher ne loh
                   </Typography>
                </Link>
 
